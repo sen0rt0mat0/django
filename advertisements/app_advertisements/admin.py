e6c760e242a07a377cba5a303aa1d55c952007ea
Sen0rT0mat0 <starmarker10@gmail.com>
2023-08-09 21:38:30 +0700

    lessons_8

--- advertisements/app_advertisements/admin.py
+++ advertisements/app_advertisements/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Advertisement
+class AdvertisementAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'description', 'created_date', 'updated_date', 'auction']
+    list_filter = ['created_at', 'auction']
+
+admin.site.register(Advertisement, AdvertisementAdmin)
 
 # Register your models here.
--- advertisements/app_advertisements/models.py
+++ advertisements/app_advertisements/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from django.contrib import admin
+from django.utils import timezone
+from django.utils.html import format_html
 
 # Create your models here.
 class Advertisement(models.Model):
@@ -8,9 +11,27 @@ class Advertisement(models.Model):
     auction = models.BooleanField('Торг', help_text='Укажите, уместен ли торг')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-
-    class Meta:
-        db_table = 'Advertisement'
-
     def __str__(self):
         return f"Advertisement(id={self.id}, title={self.title}, price={self.price})"
+
+    @admin.display(description='Дата создания')
+    def created_date(self):
+        if self.created_at.date() == timezone.now().date():
+            created_time = self.created_at.time().strftime('%H:%M:%S')
+            return 'Сегодня в ' + str(created_time)
+        else:
+            return self.created_at.strftime('%d.%m.%Y в %H:%M:%S')
+
+    @admin.display(description='Дата изменения')
+    def updated_date(self):
+        if self.updated_at.date() == timezone.now().date():
+            updated_time = self.updated_at.time().strftime('%H:%M:%S')
+            return format_html(
+                '<span style = "color: green">Сегодня в {}</span>', updated_time
+            )
+        else:
+            return self.updated_at.strftime('%d.%m.%Y в %H:%M:%S')
+
+
+class Meta:
+    db_table = 'Advertisement'
